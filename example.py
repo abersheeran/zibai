ca2b5e0d10fba6b21e34fed2eb57edf713330b93
abersheeran <me@abersheeran.com>
2023-12-10 22:50:04 +0800

    Add logging and hooks to example.py

--- example.py
+++ example.py
@@ -1,8 +1,25 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 def app(environ, start_response):
     status = "200 OK"
     headers = [("Content-type", "text/plain; charset=utf-8"), ("Content-Length", "12")]
     start_response(status, headers)
     return [b"Hello World!"]
+
+
+def before_serve_hook():
+    logger.info("Starting server...")
+
+
+def before_graceful_exit_hook():
+    logger.info("Graceful exit...")
+
+
+def before_died_hook():
+    logger.info("Died...")
 
 
 if __name__ == "__main__":
