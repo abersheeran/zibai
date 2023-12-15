3c5ad2da4c68b3e60110e94bf8cedd656c6704a9
abersheeran <me@abersheeran.com>
2023-12-15 16:49:41 +0800

    Update example.py

--- example.py
+++ example.py
@@ -4,10 +4,24 @@ logger = logging.getLogger(__name__)
 
 
 def app(environ, start_response):
-    status = "200 OK"
-    headers = [("Content-type", "text/plain; charset=utf-8"), ("Content-Length", "12")]
-    start_response(status, headers)
-    return [b"Hello World!"]
+    match environ["REQUEST_METHOD"]:
+        case "POST":
+            status = "200 OK"
+            headers = []
+            start_response(status, headers)
+            for chunk in environ["wsgi.input"]:
+                if chunk:
+                    yield chunk
+                else:
+                    return
+        case _:
+            status = "200 OK"
+            headers = [
+                ("Content-type", "text/plain; charset=utf-8"),
+                ("Content-Length", "12"),
+            ]
+            start_response(status, headers)
+            yield b"Hello World!"
 
 
 def before_serve_hook():
