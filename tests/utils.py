e24f3c441727b091d106c9e32a9bc03462fcb418
abersheeran <me@abersheeran.com>
2024-03-08 16:07:00 +0800

    Use @new_console_in_windows

--- tests/utils.py
+++ tests/utils.py
@@ -1,29 +1,24 @@
+import functools
 import os
-import random
-
-from zibai import create_bind_socket
+from typing import Any, Callable
 
 
-def create_ipv4_socket():
-    for _ in range(10):
-        port = random.randint(20000, 60000)
-        try:
-            return create_bind_socket(f"127.0.0.1:{port}")
-        except IOError:
-            continue
+def new_console_in_windows(test_function: Callable[[], Any]):
+    if os.name != "nt":
+        return test_function
 
+    @functools.wraps(test_function)
+    def new_function():
+        import subprocess
+        import sys
 
-def create_ipv6_socket():
-    for _ in range(10):
-        port = random.randint(20000, 60000)
-        try:
-            return create_bind_socket(f"::1:{port}")
-        except IOError:
-            continue
+        return subprocess.check_call(
+            [
+                sys.executable,
+                "-c",
+                f"from {test_function.__module__} import {test_function.__name__}; {test_function.__name__}.__wrapped__()",
+            ],
+            creationflags=subprocess.CREATE_NO_WINDOW,
+        )
 
-
-def create_unix_socket():
-    path = "/tmp/test-zibai-server.sock"
-    if os.path.exists(path):
-        os.remove(path)
-    return create_bind_socket(f"unix:{path}")
+    return new_function
