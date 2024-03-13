eb4f81ad810af457c62e6ace1ebcf6974bad485b
abersheeran <me@abersheeran.com>
2024-03-13 11:18:03 +0800

    Fix sys.path.insert() to use os.getcwd() instead of "."

--- src/zibai/__main__.py
+++ src/zibai/__main__.py
@@ -1,10 +1,11 @@
+import os
 import sys
 
 from .cli import main, parse_args
 
 
 def command_line() -> None:
-    sys.path.insert(0, ".")
+    sys.path.insert(0, os.getcwd())
     main(parse_args(sys.argv[1:]))
 
 
